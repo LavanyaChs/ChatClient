9cecc454df9dea76421509fc4f1ff5752b6f854a
LavanyaChs <chslavanya@yahoo.com>
2021-02-21 20:13:36 +0530

    first commit

---
+++ src/components/InfoBar/InfoBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './InfoBar.css';
+import closeIcon from '../../icons/closeIcon.png';
+import ononline from '../../icons/onlineIcon.png'
+const InfoBar = ({ room }) => (
+    <div className='infoBar'>
+        <div className='leftInnerContainer'>
+            <img className='onlineIcon' src={ononline} />
+            <h3>{room}</h3>
+        </div>
+        <div className='rightInnerContainer'>
+            <a href='/'><img src={closeIcon} alt='close imae' /></a>
+        </div></div>
+)
+export default InfoBar;
